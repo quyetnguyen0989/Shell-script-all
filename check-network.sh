bb4039a34fdc45497d716e2b9d3e68f14f3b7694
quyetnguyen0989 <quyetnguyen0989@gmail.com>
2018-05-24 11:16:47 +0700

    Update check-network.sh

--- check-network.sh
+++ check-network.sh
@@ -1,93 +1,103 @@
 #!bin/bash
-  up1="Network Mindgeek and 5th floor Viettel 172.16.10.0 is Up"
-  up2="Network MI routing out Main VNPT FTTH 172.16.11.0 is Up"
-  up3="Network Titan and Auvenir 172.16.12.0 is Up"
-  up4="Network Server routing out Main VNPT FTTH 192.168.1.0 is Up"
+  up1="Subnet 172.16.10.0/24 route to WAN Viettel - 115.78.92.111 is Up"
+  up2="Subnet 172.16.11.0/24 route to WAN VNPT - 123.21.33.153 is Up"
+  up3="Subnet 172.16.12.0/24 route to WAN VNPT - 123.21.33.154 is Up"
+  up4="Subnet 192.168.1.0/24 route to WAN VNPT - 123.21.33.153 is Up"
   up5="Lease line 192.168.6.1 is Up"
   up6="Lease line 10.100.76.36 is Up"
   up7="Lease line 10.100.78.181 is Up"
   up8="tms.titancorpvn.com is Up"
   up9="tis.titancorpvn.com is Up"
+  allup="Today: All of Services up"
 
-  down1="Network Mindgeek and 5th floor Viettel 172.16.10.0 is Down"
-  down2="Network MI routing out Main VNPT FTTH 172.16.11.0  is Down"
-  down3="Network Titan and Auvenir 172.16.12.0 is Down"
-  down4="Network Server routing out Main VNPT FTTH 192.168.1.0 192.168.1.0 is Down"
+  down1="Subnet 172.16.10.0/24 route to WAN Viettel - 115.78.92.111 is Down"
+  down2="Subnet 172.16.11.0/24 route to WAN VNPT - 123.21.33.153 is Down"
+  down3="Subnet 172.16.12.0/24 route to WAN VNPT - 123.21.33.154 is Down"
+  down4="Subnet 192.168.1.0/24 route to WAN VNPT - 123.21.33.153 is Down"
   down5="Lease line 192.168.6.1 is Down"
   down6="Lease line 10.100.76.36 is Down"
   down7="Lease line 10.100.78.181 is Down"
   down8="tms.titancorpvn.com is Down"
   down9="tis.titancorpvn.com is Down"
   https="https://tis.titancorpvn.com"
+  downcase="Today: Service Errors please check bellow"
+ 
 
 
    if ping -I 172.16.10.2 google.com -c 3 >/dev/null; then
-           echo $up1  > /home/quyetnguyen/Mindgeek-5th-floor-Viettel
+           echo $up1  > /home/quyetnguyen/files/Subnet-172.16.10.0-Viettel
    else
-           echo $down1  > /home/quyetnguyen/Mindgeek-5th-floor-Viettel
+           echo $down1  > /home/quyetnguyen/files/Subnet-172.16.10.0-Viettel
    fi
 
 
    if ping -I 172.16.11.2 google.com -c 3 >/dev/null; then
-           echo $up2  > /home/quyetnguyen/MI
+           echo $up2  > /home/quyetnguyen/files/Subnet-172.16.11.0-VNPT
    else
-           echo $down2  > /home/quyetnguyen/MI
+           echo $down2  > /home/quyetnguyen/files/Subnet-172.16.11.0-VNPT
    fi
 
 
   if ping -I 172.16.12.2 google.com -c 3 >/dev/null; then
-           echo $up3  > /home/quyetnguyen/Titan-and-Auvenir
+           echo $up3  > /home/quyetnguyen/files/Subnet-172.16.12.0-VNPT
    else
-           echo $down3  > /home/quyetnguyen/Titan-and-Auvenir
+           echo $down3  > /home/quyetnguyen/files/Subnet-172.16.12.0-VNPT
    fi
 
 
   if ping -I 192.168.1.2 google.com -c 3 >/dev/null; then
-           echo $up4  > /home/quyetnguyen/Server-routing-out-Main-VNPT-FTTH
+           echo $up4  > /home/quyetnguyen/files/Subnet-192.168.1.0-VNPT
    else
-           echo $down4  > /home/quyetnguyen/Server-routing-out-Main-VNPT-FTTH
+           echo $down4  > /home/quyetnguyen/files/Subnet-192.168.1.0-VNPT
    fi
 
   
   if ping -c3 192.168.6.1  >/dev/null; then
-           echo $up5  > /home/quyetnguyen/Lease-line-1
+           echo $up5  > /home/quyetnguyen/files/Lease-line-1
    else
-           echo $down5  > /home/quyetnguyen/Lease-line-1
+           echo $down5  > /home/quyetnguyen/files/Lease-line-1
    fi
 
   if ping -I 172.16.10.2 10.100.76.36 -c 3 >/dev/null; then
-           echo $up6  > /home/quyetnguyen/Lease-line-36
+           echo $up6  > /home/quyetnguyen/files/Lease-line-36
    else
-           echo $down6  > /home/quyetnguyen/Lease-line-36
+           echo $down6  > /home/quyetnguyen/files/Lease-line-36
    fi
 
   
   if ping -I 172.16.10.2 10.100.78.181 -c 3 >/dev/null; then
-           echo $up7  > /home/quyetnguyen/Lease-line-181
+           echo $up7  > /home/quyetnguyen/files/Lease-line-181
    else
-           echo $down7  > /home/quyetnguyen/Lease-line-181
+           echo $down7  > /home/quyetnguyen/files/Lease-line-181
    fi
 
   
   curl -s --head  --request GET http://tms.titancorpvn.com/ > web8.txt
-    if  cat /home/quyetnguyen/web8.txt | grep "200 OK" ; then
+    if  cat /home/quyetnguyen/files/web8.txt | grep "200 OK" ; then
            
-           echo $up8  > /home/quyetnguyen/Tms
+           echo $up8  > /home/quyetnguyen/files/Tms
    else
-           echo $down8  > /home/quyetnguyen/Tms
+           echo $down8  > /home/quyetnguyen/files/Tms
    fi
 
   status=`curl $https -k -s -f -o /dev/null && echo "OK" || echo "ERROR"`
-    echo "testing $https=$status" > web9.txt
-    if  cat /home/quyetnguyen/web9.txt | grep "OK" ; then
+    echo "testing $https=$status" > /home/quyetnguyen/files/web9.txt
+    if  cat /home/quyetnguyen/files/web9.txt | grep "OK" ; then
            
-           echo $up9  > /home/quyetnguyen/Tis
+           echo $up9  > /home/quyetnguyen/files/Tis
    else
-           echo $down9  > /home/quyetnguyen/Tis
+           echo $down9  > /home/quyetnguyen/files/Tis
    fi
         
-  echo `date` > Time; 
-	tail -vn +1 Time Mindgeek-5th-floor-Viettel MI Titan-and-Auvenir Server-routing-out-Main-VNPT-FTTH Lease-line-1 Lease-line-36 Lease-line-181 Tms Tis  > sentmail.txt
- 
-	cat /home/quyetnguyen/sentmail.txt | mail -s "Report Daily of Router GW-PRI-01" quyetnguyen0989@gmail.com
+  echo `date` > /home/quyetnguyen/files/Time;
+
+   if
+    grep -rnw '/home/quyetnguyen/files/' -e "Up" ; then
+            echo $allup > /home/quyetnguyen/files/Sum
+   else
+            echo $downcase > /home/quyetnguyen/files/Sum   
+   fi
+ 	
+	cd /home/quyetnguyen/files/ && tail -vn +1 Time Sum Subnet-172.16.10.0-Viettel Subnet-172.16.11.0-VNPT Subnet-172.16.12.0-VNPT Subnet-192.168.1.0-VNPT Lease-line-1 Lease-line-36 Lease-line-181 Tms Tis  > /home/quyetnguyen/sentmail.txt	
+    	cat /home/quyetnguyen/sentmail.txt | mail -s "Report Network Daily" it@titancorpvn.com
  exit 0
